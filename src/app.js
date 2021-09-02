855f9603b1e8fbd4c2ef8fa83d7e36164ee826c0
zarandonlautaro <zarandonlautaro@mail.com>
2021-09-02 14:29:57 -0300

    change env

--- src/app.js
+++ src/app.js
@@ -2,18 +2,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.resolve(__dirname, '../public')));
 
-app.use(express.json())
+app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/', (req, res) => res.json({ clave: "con el server" }));
+app.use('/', (req, res) => res.json({ clave: 'con el server' }));
 
 app.listen(PORT, () => {
-    console.log('Servidor corriendo en el puerto' + PORT)
-}
-
-);
+  console.log('Servidor corriendo en el puerto' + PORT);
+});
